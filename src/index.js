ed9dda4c725aa0000801f99d91bfa64b013bdcca
Patrik Mäsiar <patrik@kontentino.com>
2020-10-23 18:42:31 +0200

    Remove default code

---
+++ src/index.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
