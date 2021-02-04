d8692678828880b51ee21fad06a4006c3df7d6d6
Patrik Mäsiar <patrik@kontentino.com>
2021-02-04 20:47:55 +0100

    Update .eslintrc

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, {useEffect} from 'react';
-import Router from './router'
-import RNBootSplash from "react-native-bootsplash";
+import Router from './router';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App: () => React$Node = () => {
   useEffect(() => {
     RNBootSplash.hide({duration: 450});
   }, [true]);
 
-  return (
-    <Router />
-  )
+  return <Router />;
 };
 
 export default App;
