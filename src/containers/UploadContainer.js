97fe598b6d43d3cee08c593eec2d31ccedcc930f
Patrik Mäsiar <patrik@kontentino.com>
2021-02-04 08:44:00 +0100

    Config optimized build

--- src/containers/UploadContainer.js
+++ src/containers/UploadContainer.js
@@ -1,39 +1,47 @@
 import React from 'react';
 import Main from '../components/Main';
-import { useAppContext } from '../store';
+import {useAppContext} from '../store';
 import RNFetchBlob from 'rn-fetch-blob';
 import {API_URL, BASE_URL} from '../env';
-import { SHARE_SCREEN } from '../utils/constants';
+import {SHARE_SCREEN} from '../utils/constants';
 import {Platform} from 'react-native';
 
 const UploadContainer: () => React$Node = () => {
-  const {state: {
-    selectedImages,
-    isUploading,
-  }, actions: {
-    setIsUploading,
-    setShareUrl,
-    navigateToScreen,
-  }} = useAppContext();
+  const {
+    state: {selectedImages, isUploading},
+    actions: {setIsUploading, setShareUrl, navigateToScreen},
+  } = useAppContext();
 
   const handleUploadClick = async () => {
     let dataArray = [];
     for (image of selectedImages) {
-      const fileNameByPlatform = Platform.OS === 'android' ? image.path.substring(image.path.lastIndexOf('/')+1) : image.filename;
+      const fileNameByPlatform =
+        Platform.OS === 'android'
+          ? image.path.substring(image.path.lastIndexOf('/') + 1)
+          : image.filename;
 
       dataArray.push({
         name: 'images',
         filename: fileNameByPlatform,
         type: image.mime,
-        data: RNFetchBlob.wrap(Platform.OS === 'android' ? image.path : image.path.replace("file://", "")),
+        data: RNFetchBlob.wrap(
+          Platform.OS === 'android'
+            ? image.path
+            : image.path.replace('file://', ''),
+        ),
       });
     }
 
     try {
       setIsUploading(true);
-      const response = await RNFetchBlob.fetch('POST', `${API_URL}upload`, {
-        'Content-Type' : 'multipart/form-data',
-      }, dataArray);
+      const response = await RNFetchBlob.fetch(
+        'POST',
+        `${API_URL}upload`,
+        {
+          'Content-Type': 'multipart/form-data',
+        },
+        dataArray,
+      );
       setIsUploading(false);
 
       const responseData = JSON.parse(response.data);
@@ -43,7 +51,7 @@ const UploadContainer: () => React$Node = () => {
         navigateToScreen(SHARE_SCREEN);
       }
     } catch (error) {
-        console.log('Can not upload media', error);
+      console.log('Can not upload media', error);
     }
   };
 
@@ -53,7 +61,7 @@ const UploadContainer: () => React$Node = () => {
       isLoading={isUploading}
       isUploadDisabled={selectedImages.length === 0}
     />
-  )
+  );
 };
 
 export default UploadContainer;
