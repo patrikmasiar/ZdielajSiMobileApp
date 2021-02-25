d4dc1fe519b4f1ff05d3e74aba11650193bdf5cf
Patrik Mäsiar <patrik@kontentino.com>
2021-02-25 19:59:24 +0100

    Create ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {useEffect} from 'react';
 import Router from './router';
 import RNBootSplash from 'react-native-bootsplash';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App: () => React$Node = () => {
   useEffect(() => {
     RNBootSplash.hide({duration: 450});
   }, [true]);
 
-  return <Router />;
+  return (
+    <ErrorBoundary>
+      <Router />
+    </ErrorBoundary>
+  );
 };
 
 export default App;
