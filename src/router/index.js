b0cf5fd30801ced989360f9db61e598439a8d98b
Patrik Mäsiar <patrik@kontentino.com>
2021-03-01 21:36:20 +0100

    Add header to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,39 @@
 import React, {lazy, Suspense} from 'react';
-import {ActivityIndicator, StyleSheet} from 'react-native';
+import {ActivityIndicator, StyleSheet, Text} from 'react-native';
 import {Layout} from '@ui-kitten/components';
 import {createStackNavigator} from '@react-navigation/stack';
-import Navigation from '../components/Navigation';
 
 const Share = lazy(() => import('../screens/Share'));
 const Images = lazy(() => import('../screens/ImagesList'));
 
 const Stack = createStackNavigator();
 
-const screenOptions = {
-  headerShown: false,
-};
-
 const Router = () => {
   return (
     <Layout style={style.container}>
-      <Navigation />
+      {/* <Navigation /> */}
       <Suspense fallback={<ActivityIndicator />}>
         <Stack.Navigator>
           <Stack.Screen
-            name="Upload"
+            name="upload"
             component={Images}
-            options={screenOptions}
+            title="Hello"
+            options={{
+              title: <Text style={style.title}>Zdieľaj.si</Text>,
+              headerStyle: {
+                height: 60,
+              },
+            }}
           />
           <Stack.Screen
-            name="Share"
+            name="share"
             component={Share}
-            options={screenOptions}
+            options={{
+              title: <Text style={style.title}>Zdieľaj.si</Text>,
+              headerStyle: {
+                height: 60,
+              },
+            }}
           />
         </Stack.Navigator>
       </Suspense>
@@ -38,6 +44,11 @@ const Router = () => {
 const style = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  title: {
+    fontWeight: '900',
+    fontSize: 18,
+    color: '#3255AF',
   },
 });
 
