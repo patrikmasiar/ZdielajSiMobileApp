714e2c62f7ddc5c3fef58ae2aa9033988fa0fe15
Patrik Mäsiar <patrik@kontentino.com>
2021-03-01 22:19:43 +0100

    Move old containers to deprecated folder

---
+++ src/containers/deprecated/UploadContainer.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Main from '../../components/Main';
+import {useAppContext} from '../../store';
+import RNFetchBlob from 'rn-fetch-blob';
+import {API_URL, BASE_URL} from '../../env';
+import {SHARE_SCREEN} from '../../utils/constants';
+import {Platform} from 'react-native';
+
+const UploadContainer: () => React$Node = () => {
+  const {
+    state: {selectedImages, isUploading},
+    actions: {setIsUploading, setShareUrl, navigateToScreen},
+  } = useAppContext();
+
+  const handleUploadClick = async () => {
+    let dataArray = [];
+    for (image of selectedImages) {
+      const fileNameByPlatform =
+        Platform.OS === 'android'
+          ? image.path.substring(image.path.lastIndexOf('/') + 1)
+          : image.filename;
+
+      dataArray.push({
+        name: 'images',
+        filename: fileNameByPlatform,
+        type: image.mime,
+        data: RNFetchBlob.wrap(
+          Platform.OS === 'android'
+            ? image.path
+            : image.path.replace('file://', ''),
+        ),
+      });
+    }
+
+    try {
+      setIsUploading(true);
+      const response = await RNFetchBlob.fetch(
+        'POST',
+        `${API_URL}upload`,
+        {
+          'Content-Type': 'multipart/form-data',
+        },
+        dataArray,
+      );
+      setIsUploading(false);
+
+      const responseData = JSON.parse(response.data);
+
+      if (responseData.error === null && !!responseData.data) {
+        setShareUrl(`${BASE_URL}album/${responseData.data.album.id}`);
+        navigateToScreen(SHARE_SCREEN);
+      }
+    } catch (error) {
+      console.log('Can not upload media', error);
+    }
+  };
+
+  return (
+    <Main
+      onUploadPress={handleUploadClick}
+      isLoading={isUploading}
+      isUploadDisabled={selectedImages.length === 0}
+    />
+  );
+};
+
+export default UploadContainer;
