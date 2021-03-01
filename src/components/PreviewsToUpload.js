a6ba68d448838feb0542e24f02a08619b8354293
Patrik Mäsiar <patrik@kontentino.com>
2021-03-01 22:49:52 +0100

    Add item component

--- src/components/PreviewsToUpload.js
+++ src/components/PreviewsToUpload.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, View, Image} from 'react-native';
 import EmptyPreivewsMessage from './EmptyPreviewsMessage';
+import {Button} from '@ui-kitten/components';
+
+const Item = ({item, onRemovePress}) => (
+  <View style={style.item}>
+    <Image source={{uri: item.path}} resizeMode="cover" style={style.image} />
+    <Button
+      status="danger"
+      size="tiny"
+      appearance="outline"
+      onPress={() => null}>
+      Vymazať
+    </Button>
+  </View>
+);
 
 const PreviewsToUpload = ({data}) => (
   <FlatList
     data={data}
     style={style.list}
-    renderItem={(item) => null}
+    renderItem={(item) => <Item item={item.item} />}
     keyExtractor={(item) => item.path}
     ItemSeparatorComponent={() => <View style={style.separator} />}
     ListEmptyComponent={() => <EmptyPreivewsMessage />}
@@ -14,11 +28,25 @@ const PreviewsToUpload = ({data}) => (
 );
 
 const style = StyleSheet.create({
-  list: {},
+  list: {
+    padding: 15,
+  },
   separator: {
-    height: 2,
+    height: 1,
     backgroundColor: '#fff',
     width: '100%',
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 5,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 12,
   },
 });
 
