a4d6aca215ce1c00feeb93052aecb26dd2dd2019
patrikmasiar <masiar.patrik@gmail.com>
2021-03-20 18:13:41 +0100

    Remove unused screens

--- src/screens/deprecated/ShareScreen.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ShareContainer from '../../containers/ShareContainer';
-
-const ShareScreen = () => <ShareContainer />;
-
-export default ShareScreen;
--- src/screens/deprecated/UploadScreen.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import UploadContainer from '../../containers/UploadContainer';
-
-const UploadScreen = () => <UploadContainer />;
-
-export default UploadScreen;
