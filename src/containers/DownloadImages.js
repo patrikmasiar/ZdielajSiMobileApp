1c3c48962486ac32d9fa6e3a33d94660e0325f15
patrikmasiar <masiar.patrik@gmail.com>
2021-04-01 16:00:30 +0200

    Prepare download request

--- src/containers/DownloadImages.js
+++ src/containers/DownloadImages.js
@@ -1,9 +1,12 @@
 import {useState} from 'react';
+import {downloadImages as downloadFromAPI} from '../api/Download';
 
 const DownloadImages = ({children}) => {
   const [urlValue, setUrlValue] = useState('');
 
-  const downloadImages = () => {};
+  const downloadImages = async () => {
+    downloadFromAPI(urlValue);
+  };
 
   return children({
     urlValue,
