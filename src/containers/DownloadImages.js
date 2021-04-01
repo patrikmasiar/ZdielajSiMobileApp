5f7438c85d96cb3e8e7f6e57191b426a9ffedfff
patrikmasiar <masiar.patrik@gmail.com>
2021-04-01 18:36:19 +0200

    Fetch and set images urls

--- src/containers/DownloadImages.js
+++ src/containers/DownloadImages.js
@@ -3,15 +3,27 @@ import {downloadImages as downloadFromAPI} from '../api/Download';
 
 const DownloadImages = ({children}) => {
   const [urlValue, setUrlValue] = useState('');
+  const [imagesUrls, setImagesUrls] = useState([]);
 
   const downloadImages = async () => {
-    downloadFromAPI(urlValue);
+    const data = await downloadFromAPI(urlValue);
+
+    if (data) {
+      const images = data.photos;
+      const formatted = images.map((image) => ({
+        thumbnail: image.thumbnail.location,
+        original: image.location,
+      }));
+
+      setImagesUrls(formatted);
+    }
   };
 
   return children({
     urlValue,
     onUrlChange: setUrlValue,
     downloadImages,
+    images: imagesUrls,
   });
 };
 
