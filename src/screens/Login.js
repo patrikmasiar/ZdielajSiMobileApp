3bb58177e526f5c967285c33db09c8246a9319d1
patrikmasiar <masiar.patrik@gmail.com>
2021-05-06 20:57:07 +0200

    Create new form components

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import LoginForm from '../components/LoginForm';
+import LoginContainer from '../containers/Login';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={style.wrapper}>
+      <LoginContainer>{(params) => <LoginForm />}</LoginContainer>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#fff',
+    flex: 1,
+    position: 'relative',
+  },
+});
 
 export default Login;
