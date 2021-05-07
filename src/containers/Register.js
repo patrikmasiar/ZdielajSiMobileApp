591618bbb82d720b61e432d453ea894c88fb5c7e
patrikmasiar <masiar.patrik@gmail.com>
2021-05-07 22:14:40 +0200

    Update container with api call

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,5 +1,15 @@
+import {register} from '../api/User';
+
 const RegisterContainer = ({children}) => {
-  const handleRegister = (name, email, password) => {};
+  const handleRegister = async (name, email, password) => {
+    try {
+      const response = await register(name, email, password);
+
+      console.log('RESPONSE::', response);
+    } catch (e) {
+      console.log('Error register user:', e);
+    }
+  };
 
   return children({
     register: handleRegister,
