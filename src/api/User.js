3ff50aad8106b9a441835c7299f062ae608f3e81
patrikmasiar <masiar.patrik@gmail.com>
2021-05-07 22:14:07 +0200

    Finish login and register api calls

--- src/api/User.js
+++ src/api/User.js
@@ -1,9 +1,50 @@
-// TODO:
+import {API_URL} from '../env';
 
-export const login = () => {
-  return null;
+export const login = async (email, password) => {
+  try {
+    const response = await fetch(`${API_URL}user/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (result) {
+      return result;
+    }
+  } catch (error) {
+    console.log('Can not login user', error);
+    return null;
+  }
 };
 
-export const register = () => {
-  return null;
+export const register = async (name, email, password) => {
+  try {
+    const response = await fetch(`${API_URL}user/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (result) {
+      return result;
+    }
+  } catch (error) {
+    console.log('Can not register user', error);
+    return null;
+  }
 };
