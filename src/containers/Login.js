c10519e51a8dd18504e6f04be6c247480dd7af6a
patrikmasiar <masiar.patrik@gmail.com>
2021-05-09 21:58:23 +0200

    Create login handler with api call

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,13 +1,32 @@
+import {useState} from 'react';
+import {login} from '../api/User';
+
 const LoginContainer = ({children, navigation}) => {
+  const [isLoading, setLoading] = useState(false);
+
   const handleGoToRegister = () => {
     navigation.navigate('registration');
   };
 
-  const handleLogin = (name, email) => {};
+  const handleLogin = async (email, password) => {
+    try {
+      setLoading(true);
+      const response = await login(email, password);
+
+      if (response && !!response.data.user.id) {
+        // TODO: save user token
+      }
+    } catch (e) {
+      console.log('Error login user:', e);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return children({
     goToRegister: handleGoToRegister,
     login: handleLogin,
+    isLoading,
   });
 };
 
