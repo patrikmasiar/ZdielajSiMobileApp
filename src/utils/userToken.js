a9b02842c635bdaafa88810848b36983e1f521a1
patrikmasiar <masiar.patrik@gmail.com>
2021-05-12 18:35:00 +0200

    Create UserToken storage helper

---
+++ src/utils/userToken.js
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const UserToken = {
+  get: async () => {
+    try {
+      const userToken = await AsyncStorage.getItem('stored_location_data');
+
+      return userToken;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  set: async (token) => {
+    try {
+      await AsyncStorage.setItem('user_token', token);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+  remove: async () => {
+    try {
+      await AsyncStorage.setItem('user_token', null);
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
+
+export default UserToken;
