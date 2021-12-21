1d9bb277be4a8158ae221a8bf8b42aebc09ae636
patrikmasiar <masiar.patrik@gmail.com>
2021-12-21 23:25:43 +0100

    Change alert message

--- src/containers/PreviewsContainer.js
+++ src/containers/PreviewsContainer.js
@@ -20,7 +20,7 @@ const PreviewsContainer = ({children}) => {
         setPreviews(pickerResponse);
       } else {
         Alert.alert(
-          'Nepodarilo sa nahrať súbory z knižnice',
+          'Nepodarilo sa nahrať médiá z knižnice',
           'Prosím, skúste znovu',
         );
       }
