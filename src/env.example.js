5c82de369c7c9570c6d2a7fbb0eefee97c9d991b
patrikmasiar <masiar.patrik@gmail.com>
2021-12-22 11:26:11 +0100

    Fix env

--- src/env.example.js
+++ src/env.example.js
@@ -3,7 +3,7 @@ export const BASE_URL = '';
 export const SENTRY_DNS = '';
 
 export const CONFIG = {
-  ALLOWED_MEDIA_TYPES_NOT_SIGNED: 'photos',
+  ALLOWED_MEDIA_TYPES_NOT_SIGNED: 'photo',
   ALLOWED_MEDIA_TYPES_SIGNED: 'any',
   NOT_SIGNED_MEDIA_UPLOAD_LIMIT: 10,
 };
