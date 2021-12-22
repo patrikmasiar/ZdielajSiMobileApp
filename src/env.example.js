ee40ac7ec481fd4966a3a8a77bd4574f2a8d4f2c
patrikmasiar <masiar.patrik@gmail.com>
2021-12-22 10:13:57 +0100

    Update env.example

--- src/env.example.js
+++ src/env.example.js
@@ -1,3 +1,9 @@
 export const API_URL = '';
 export const BASE_URL = '';
 export const SENTRY_DNS = '';
+
+export const CONFIG = {
+  ALLOWED_MEDIA_TYPES_NOT_SIGNED: 'photos',
+  ALLOWED_MEDIA_TYPES_SIGNED: 'any',
+  NOT_SIGNED_MEDIA_UPLOAD_LIMIT: 10,
+};
