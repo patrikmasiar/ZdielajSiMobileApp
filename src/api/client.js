6fcc3183641d8a58eae87e70502926593ce74bd9
patrikmasiar <masiar.patrik@gmail.com>
2022-08-20 19:41:24 +0200

    Fix post request headers

--- src/api/client.js
+++ src/api/client.js
@@ -5,21 +5,12 @@ export async function get(url, options) {
 }
 
 export async function post(url, options) {
-  let headers = {
-    'Content-Type': 'application/json',
-  };
-
-  if (options?.headers) {
-    headers = {
-      ...headers,
-      ...options.headers,
-    };
-  }
-
   return fetch(`${API_URL}${url}`, {
     method: 'POST',
-    headers,
+    headers: {
+      'Content-Type': 'application/json',
+      ...options?.headers,
+    },
     body: options?.data ? JSON.stringify(options.data) : undefined,
-    ...options,
   }).then((res) => res.json());
 }
