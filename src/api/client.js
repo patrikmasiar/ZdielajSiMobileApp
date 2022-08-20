2545162d7609f762abc646f204691cd7ffdf6a43
patrikmasiar <masiar.patrik@gmail.com>
2022-08-20 17:55:55 +0200

    Create API client get, post functions

---
+++ src/api/client.js
@@ -0,0 +1,16 @@
+import {API_URL} from '../env';
+
+export async function get(url, options) {
+  return fetch(`${API_URL}${url}`, options);
+}
+
+export async function post(url, options) {
+  return fetch(`${API_URL}${url}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(options.data),
+    ...options,
+  }).then((res) => res.json());
+}
