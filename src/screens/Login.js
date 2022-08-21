a27e4535796d74e4969ad9ae987e8ece3e391470
patrikmasiar <masiar.patrik@gmail.com>
2022-08-21 16:49:08 +0200

    Add privacy policy link to login screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import {ScrollView, StyleSheet, Linking} from 'react-native';
 import LoginForm from '../components/LoginForm';
 import LoginContainer from '../containers/Login';
+import {Button} from '@ui-kitten/components';
+import {BASE_URL} from '../env';
 
 const Login = ({navigation}) => {
   return (
@@ -15,6 +17,12 @@ const Login = ({navigation}) => {
           />
         )}
       </LoginContainer>
+      <Button
+        appearance="ghost"
+        size="small"
+        onPress={() => Linking.openURL(`${BASE_URL}o-aplikacii`)}>
+        Podmienky používania
+      </Button>
     </ScrollView>
   );
 };
