52a0694fa71b54a83db010a754e0a6c79a55efac
patrikmasiar <masiar.patrik@gmail.com>
2022-10-06 21:49:37 +0200

    Create requestDelete function

--- src/api/client.js
+++ src/api/client.js
@@ -4,6 +4,17 @@ export async function get(url, options) {
   return fetch(`${API_URL}${url}`, options).then((res) => res.json());
 }
 
+export async function requestDetele(url, options) {
+  return fetch(`${API_URL}${url}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...options?.headers,
+    },
+    body: options?.data ? JSON.stringify(options.data) : undefined,
+  }).then(res => res.json());
+}
+
 export async function post(url, options) {
   return fetch(`${API_URL}${url}`, {
     method: 'POST',
